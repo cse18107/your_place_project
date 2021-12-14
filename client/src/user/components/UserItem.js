2e7ad174358edb755b6b1aa10c07fe1ed02b4864
cse18107 <cse18107@cemk.ac.in>
2021-12-14 12:21:24 +0530

    Styling our App & More Components

--- client/src/user/components/UserItem.js
+++ client/src/user/components/UserItem.js
@@ -1,21 +1,27 @@
-import React from 'react'
-import './UserItem.css'
-
+import React from "react";
+import "./UserItem.css";
+import Card from "../../shared/components/UIElements/Card";
+import { Link } from "react-router-dom";
+import Avatar from "../../shared/components/UIElements/Avatar";
 
 const UsersItem = (props) => {
-    return (
-        <li className="user-item">
-            <div className="user-item__content">
-                <div className="user-item__image">
-                    <img src={props.image} alt={props.name}/>
-                </div>
-                <div className="user-item-info">
-                    <h2>{props.name}</h2>
-                    <h3>{props.placeCount} {props.placeCount === 1?'Place':'Places'}</h3>
-                </div>
-            </div>
-        </li>
-    )
-}
+  return (
+    <li className="user-item">
+      <Card className="user-item__content">
+        <Link to={`/${props.id}/places`}>
+          <div className="user-item__image">
+            <Avatar image={props.image} alt={props.name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{props.name}</h2>
+            <h3>
+              {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
+            </h3>
+          </div>
+        </Link>
+      </Card>
+    </li>
+  );
+};
 
 export default UsersItem;
