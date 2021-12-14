86fced361c2022b489debda89d1fe5922230dc54
cse18107 <cse18107@cemk.ac.in>
2021-12-14 11:20:35 +0530

    40. Adding a UserList Page / Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
-
-import './App.css';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ client/src/user/pages/Users.js
@@ -0,0 +1,18 @@
+import React from "react";
+import UsersList from "../components/UsersList";
+
+const Users = () => {
+  const USERS = [
+    {
+      id: "u1",
+      name: "Soumodeep Maity",
+      image:
+        "https://i.ibb.co/pXfZN2t/irene-strong-v2a-Knj-Mb-P-k-unsplash.jpg",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
+};
+
+export default Users;
