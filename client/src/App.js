224f371a2130d2e3cec21725b64af9dcb40f40a2
cse18107 <cse18107@cemk.ac.in>
2021-12-14 14:42:14 +0530

    Adding a Main Header

--- client/src/App.js
+++ client/src/App.js
@@ -7,19 +7,23 @@ import {
 } from "react-router-dom";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 }
--- client/src/user/components/UsersList.js
+++ client/src/user/components/UsersList.js
@@ -1,12 +1,14 @@
 import React from "react";
+import Card from "../../shared/components/UIElements/Card";
 import UsersItem from "./UserItem";
 import "./UsersList.css";
+
 
 const UsersList = (props) => {
   if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2>No users found.</h2>
+        <Card><h2>No user found.</h2></Card>
       </div>
     );
   }
