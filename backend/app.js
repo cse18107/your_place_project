7aaa44c105ee87ad71bafea073b8eb80f00827d7
cse18107 <cse18107@cemk.ac.in>
2022-01-09 20:53:27 +0530

    last commit in my project

--- backend/app.js
+++ backend/app.js
@@ -53,7 +53,7 @@ mongoose
     useNewUrlParser: true
   })
   .then(() => {
-    app.listen(5000,()=>{
+    app.listen(process.env.PORT || 5000,()=>{
       console.log("DB connection successfully.")
     });
   })
--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -83,7 +83,7 @@ const PlaceItem = props => {
         <Card className="place-item__content">
           {isLoading && <LoadingSpinner asOverlay />}
           <div className="place-item__image">
-            <img src={ `${process.env.REACT_APP_BACKEND_URL}/${props.image}`} alt={props.title} />
+            <img src={ `${process.env.REACT_APP_ASSET_URL}/${props.image}`} alt={props.title} />
           </div>
           <div className="place-item__info">
             <h2>{props.title}</h2>
